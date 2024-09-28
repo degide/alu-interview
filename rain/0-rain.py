d09a0be30d894f5aca53e5d3fed3c0fcacc2d406
Harerimana Egide <egideharerimana085@gmail.com>
2024-09-28 18:39:35 +0200

    Added README

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -1,34 +1,33 @@
 #!/usr/bin/python3
-
 """
 Python Module to calculate how much rainwater is retained after it rains.
 """
 
 
-def rain(waterWalls):
+def rain(water_walls):
     """
     Calculate the square units of water will be retained after it rains.
 
     Arguments:
-    waterWalls -- list of non-negative integers representing wall heights
+    water_walls -- list of non-negative integers representing wall heights
 
     Returns:
     Integer indicating total amount of rainwater retained.
     """
-    if not waterWalls:
+    if not water_walls:
         return 0
 
-    left, right = 0, len(waterWalls) - 1
+    left, right = 0, len(water_walls) - 1
     left_max, right_max = 0, 0
     water = 0
 
     while left < right:
-        if waterWalls[left] <= waterWalls[right]:
-            left_max = max(left_max, waterWalls[left])
-            water += left_max - waterWalls[left]
+        if water_walls[left] <= water_walls[right]:
+            left_max = max(left_max, water_walls[left])
+            water += left_max - water_walls[left]
             left += 1
         else:
-            right_max = max(right_max, waterWalls[right])
-            water += right_max - waterWalls[right]
+            right_max = max(right_max, water_walls[right])
+            water += right_max - water_walls[right]
             right -= 1
     return water
