5aa2a6ffaa181836d3d83094c46a9d5101b2b407
Harerimana Egide <egideharerimana085@gmail.com>
2024-10-13 22:29:29 +0200

    Fix

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -3,6 +3,7 @@
 Pascal's triangle
 """
 
+
 def pascal_triangle(n):
     """
     Return a list of lists contained in a pascal's triangle of n
@@ -15,15 +16,11 @@ def pascal_triangle(n):
     """
     if n <= 0:
         return []
-    
     lst = [[1]]
-    
     for i in range(1, n):
         row = [1]
         for j in range(1, i):
             row.append(lst[i-1][j-1] + lst[i-1][j])
         row.append(1)
         lst.append(row)
-    
     return lst
-    
